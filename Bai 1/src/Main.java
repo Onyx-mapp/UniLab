76bbe97d36b0b6c8f018c306ea548d5d6dd6d982
Onyx-mapp <killianonyx21@gmail.com>
2025-03-04 08:46:39 +0700

    Delete Bai 1 directory

--- Bai 1/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n >= 0) System.out.println("Đây là số nguyên dương");
-        else System.out.println("Đây là số nguyên âm");
-    }
-}
